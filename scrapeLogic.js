d0bb273e975fb9686e68dd9447c7e81b8b8eb1c5
Ye Min Htwe <yeminhtwe@Yes-MacBook-Pro.local>
2025-04-01 01:38:02 +0630

    first commit

---
+++ scrapeLogic.js
@@ -0,0 +1,36 @@
+const puppeteer = require("puppeteer")
+
+const scrapeLogic = async(res) => {
+    const browser = await puppeteer.launch();
+    try {
+        const page = await browser.newPage();
+        // Navigate the page to a URL.
+        await page.goto('https://developer.chrome.com/');
+
+        // Set screen size.
+        await page.setViewport({width: 1080, height: 1024});
+
+        // Type into search box.
+        await page.locator('.devsite-search-field').fill('automate beyond recorder');
+
+        // Wait and click on first result.
+        await page.locator('.devsite-result-item-link').click();
+
+        // Locate the full title with a unique string.
+        const textSelector = await page
+        .locator('text/Customize and automate')
+        .waitHandle();
+        const fullTitle = await textSelector?.evaluate(el => el.textContent);
+
+        // Print the full title.
+        console.log('The title of this blog post is "%s".', fullTitle);
+        res.send(fullTitle)
+    } catch (e) {
+        console.error(e);
+        res.send(`Something went wrong while running: ${error}`)
+    } finally {
+        await browser.close();
+    }
+}
+
+module.exports={scrapeLogic}
