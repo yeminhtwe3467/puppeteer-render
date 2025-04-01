ea27598a062ff83912e1bfa9c94b7c1c2c1c19cc
Ye Min Htwe <yeminhtwe@Yes-MacBook-Pro.local>
2025-04-01 11:17:34 +0630

    Final one and updated

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 
 const scrapeLogic = async(res) => {
     const browser = await puppeteer.launch({
+        headless:'new',
         args: [
             "--disable-setuid-sandbox",
             "--no-sandbox",
